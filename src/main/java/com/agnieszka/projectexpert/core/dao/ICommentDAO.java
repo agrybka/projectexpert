aa7f2c62e2de427c1d366d8af655fc5a92076371
aarybka <agnieszkaagarybka@gmail.com>
2016-09-10 10:51:38 +0200

    Dodano komentarz

--- src/main/java/com/agnieszka/projectexpert/core/dao/ICommentDAO.java
+++ src/main/java/com/agnieszka/projectexpert/core/dao/ICommentDAO.java
@@ -1,7 +1,11 @@
 package com.agnieszka.projectexpert.core.dao;
 
 import com.agnieszka.projectexpert.core.domain.Comment;
-
+/**
+ * 
+ * @author Aga
+ *
+ */
 public interface ICommentDAO {
 	Comment creat(Comment comment);
 }
